58fa8720c2a3d45a49b8616b0e8d74e0cff01c4f
Serhii Liubeznov <s.liubeznov@aspanta.com>
2017-04-28 20:33:37 +0300

    [Task #3149] add new ui for name info

--- EmercoinBasic/Views/MyRecords/RecordInfoVIew.swift
+++ EmercoinBasic/Views/MyRecords/RecordInfoVIew.swift
@@ -5,9 +5,12 @@
 
 import UIKit
 
-class RecordInfoVIew: PopupView, UIScrollViewDelegate {
+class RecordInfoVIew: PopupView {
     
-    @IBOutlet internal weak var textView:BaseTextView!
+    @IBOutlet internal weak var nameTextView:BaseTextView!
+    @IBOutlet internal weak var valueTextView:BaseTextView!
+    @IBOutlet internal weak var addressLabel:UILabel!
+    @IBOutlet internal weak var expiresLabel:UILabel!
     
     var viewModel:RecordViewModel? {
         didSet {
@@ -15,50 +18,25 @@ class RecordInfoVIew: PopupView, UIScrollViewDelegate {
         }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
     private func updateUI() {
         
         if let viewModel = viewModel {
             
-            
-            let count = isIphone5() ? 33 : 44
-            
-            var separator = ""
-            
-            for _ in 0...count {
-                separator = separator+"-"
-            }
-            
             let date = String(format:"%@ %@",viewModel.expiresInDay, viewModel.expiresType)
             let name = viewModel.name
             let value =  String.isInfoCardType(at: name) ? "<Infocard>" : viewModel.value
-            
-            let text = String(format:"%@\n%@\n%@\n%@\n%@\n%@\n%@",name,separator,value,separator,
-                              viewModel.address,separator,date)
-            textView.text = text
+            let address = viewModel.address
+    
+            nameTextView.text = name
+            valueTextView.text = value
+            addressLabel.text = address
+            expiresLabel.text = date
             
         }
-    }
-    
-    override func didMoveToSuperview() {
+        
         DispatchQueue.main.asyncAfter(deadline: .now()) {
-            self.textView.isScrollEnabled = true
-            
-            for view in self.textView.subviews {
-                if let imageView = view as? UIImageView {
-                    if imageView.alpha == 0.0 && imageView.autoresizingMask == .flexibleLeftMargin {
-                        imageView.alpha = 1.0
-                        imageView.frame.size.height = 128.0
-                    }
-                }
-            }
+            self.valueTextView.isScrollEnabled = true
+            self.nameTextView.isScrollEnabled = true
         }
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("123")
     }
 }
